285e1558d7051674b61295c77afeba15ecf351b8
mokkeee <mtg-gh-99078>
2015-12-23 12:44:14 +0900

    課題4-2 solved.

--- grid_points.py
+++ grid_points.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from grid_point import GridPoint
 
 __author__ = 'mokkeee'
 
 
 class GridPoints(object):
     def __init__(self, *args):
-        if self._contains_same_grid(args):
+        if not self._is_valid_grids(args):
             raise ValueError
         self._grids = args
 
     def __contains__(self, item):
+        if not isinstance(item, GridPoint):
+            return False
         return item in self._grids
 
-    def is_connected_points(self):
-        return self._grids[0].is_neighbor(self._grids[1])
+    def __len__(self):
+        return len(self._grids)
+
+    def is_connected_grids(self):
+        for grid in self._grids:
+            if not self._has_neighbor(grid):
+                return False
+        return True
+        # return self._grids[0].is_neighbor(self._grids[1])
+
+    def _has_neighbor(self, grid):
+        neighbors = [x for x in self._grids if x.is_neighbor(grid)]
+        if len(neighbors) == 0:
+            return False
+        return True
 
     @staticmethod
-    def _contains_same_grid(args):
-        if len(set(args)) != len(args):
-            return True
-        return False
+    def _is_valid_grids(args):
+        for point in args:
+            if not isinstance(point, GridPoint):
+                return False
+        points_count = len(args)
+        if len(set(args)) != points_count:
+            return False
+        if points_count not in [2, 3]:
+            return False
+        return True
--- test_grid_points.py
+++ test_grid_points.py
@@ -26,6 +26,19 @@ class Test_GridPoints_Error:
         with pytest.raises(ValueError):
             GridPoints(GridPoint(0, 1), GridPoint(0, 1))
 
+    def test_格子点1つの格子点集合は生成できないこと(self):
+        with pytest.raises(ValueError):
+            GridPoints(GridPoint(0, 0))
+
+    def test_格子点4つの格子点集合は生成できないこと(self):
+        with pytest.raises(ValueError):
+            GridPoints(GridPoint(0, 0), GridPoint(1, 1), GridPoint(2, 2), GridPoint(3, 3))
+
+    def test_格子点以外を含んだ格子点集合は生成できないこと(self):
+        with pytest.raises(ValueError):
+            GridPoints(GridPoint(0, 1), [1, 2])
+
+
 @pytest.mark.parametrize(('grid1', 'grid2', 'result'), [
     ([1, 2], [1, 1], True),
     ([1, 2], [1, 3], True),
@@ -35,6 +48,31 @@ class Test_GridPoints_Error:
     ([0, 2], [1, 2], True),
     ([0, 2], [-1, 2], True),
 ])
-def test_格子点集合の判定ができること(grid1, grid2, result):
-    grid_points = GridPoints(GridPoint(grid1[0], grid1[1]), GridPoint(grid2[0], grid2[1]))
-    assert grid_points.is_connected_points() == result
+def test_２つの格子点を持つ格子点集合の連結判定ができること(grid1, grid2, result):
+    grid_points = GridPoints(
+        GridPoint(grid1[0], grid1[1]),
+        GridPoint(grid2[0], grid2[1]))
+    assert grid_points.is_connected_grids() == result
+
+
+class Test_3つの格子点を持つ格子点集合:
+    def test_3つの格子点をもつ格子点集合が生成できること(self):
+        points = GridPoints(GridPoint(0, 0), GridPoint(1, 1), GridPoint(2, 2))
+        assert len(points) == 3
+
+@pytest.mark.parametrize(('grid1', 'grid2', 'grid3', 'result'), [
+    ([1, 2], [1, 3], [1, 4], True),
+    ([1, 2], [1, 0], [1, 1], True),
+    ([1, 2], [2, 2], [3, 2], True),
+    ([1, 2], [3, 2], [2, 2], True),
+    ([1, 1], [2, 2], [2, 1], True),
+    ([1, 2], [2, 2], [3, 3], False),
+    ([1, 2], [3, 3], [2, 2], False),
+    ([1, 2], [3, 2], [3, 3], False),
+])
+def test_３つの格子点を持つ格子点集合の連結判定ができること(grid1, grid2, grid3, result):
+    grid_points = GridPoints(
+        GridPoint(grid1[0], grid1[1]),
+        GridPoint(grid2[0], grid2[1]),
+        GridPoint(grid3[0], grid3[1]))
+    assert grid_points.is_connected_grids() == result
